9f24c2f8feab33e843a10ffcc9b64b5dbbd4f5a4
mboras <borasmislav@gmail.com>
2015-03-31 11:26:51 +0200

    konacni komit
    kraj

--- Lutrija/Lutrija/Form1.cs
+++ Lutrija/Lutrija/Form1.cs
@@ -12,9 +12,46 @@ namespace Lutrija
 {
     public partial class frmGLavnaForma : Form
     {
+        private Loto loto;
         public frmGLavnaForma()
         {
             InitializeComponent();
+            loto = new Loto();
         }
+
+        private void btnUplati_Click(object sender, EventArgs e)
+        {
+            List<string> vrijednosti = new List<string>();
+            vrijednosti.Add(txtUplaceniBroj1.Text);
+            vrijednosti.Add(txtUplaceniBroj2.Text);
+            vrijednosti.Add(txtUplaceniBroj3.Text);
+            vrijednosti.Add(txtUplaceniBroj4.Text);
+            vrijednosti.Add(txtUplaceniBroj5.Text);
+            vrijednosti.Add(txtUplaceniBroj6.Text);
+            vrijednosti.Add(txtUplaceniBroj7.Text);
+
+            bool ispravnaKombinacija = loto.UnesiUplaceneBrojeve(vrijednosti);
+            if (ispravnaKombinacija == true)
+            {
+                btnOdigraj.Enabled = true;
+                MessageBox.Show("Uplaćeno!");
+
+            }
+            else
+            {
+                btnOdigraj.Enabled = false;
+                MessageBox.Show("Kombinacija uplaćenih brojeva nije ispravna!");
+            }
+
+        }
+
+        private void btnOdigraj_Click(object sender, EventArgs e)
+        {
+            loto.GenerirajDobitnuKombinaciju();
+
+            txtDobitniBroj1.Text=loto.dobitni
+        }
+
+
     }
 }
---
+++ Lutrija/Lutrija/Loto.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lutrija
+{
+    class Loto
+    {
+        private List<int> UplaceniBrojevi { get; set; }
+        private List<int> DobitniBrojevi { get; set; }
+
+        /// <summary>
+        /// Konstruktor klase
+        /// </summary>
+        public Loto()
+        {
+            UplaceniBrojevi = new List<int>();
+            DobitniBrojevi = new List<int>();
+        }
+
+
+        public bool UnesiUplaceneBrojeve(List<string> korisnickeVrijednosti)
+        {
+            bool ispravni = false;
+            UplaceniBrojevi.Clear();
+
+            foreach (string v in korisnickeVrijednosti)
+            {
+                int broj = 0;
+                if (int.TryParse(v, out broj) == true)
+                {
+                    if (broj >= 1 && broj <= 39)
+                    {
+                        if (UplaceniBrojevi.Contains(broj) == false)
+                        {
+                            UplaceniBrojevi.Add(broj);
+                        }
+                    }
+                }
+            }
+            if (UplaceniBrojevi.Count == 7)
+            {
+                ispravni = true;
+            }
+
+
+            return ispravni;
+        }
+
+        public void GenerirajDobitnuKombinaciju()
+        {
+            DobitniBrojevi.Clear();
+
+            Random generiraj = new Random();
+
+            while (DobitniBrojevi.Count <7)
+            {
+                int broj = generiraj.Next(1, 39);
+                if (DobitniBrojevi.Contains(broj) == false)
+                {
+                    DobitniBrojevi.Add(broj);
+                }
+            }
+
+        }
+
+        public int IzracunajBrojPogodaka()
+        {
+            int pogodak = 0;
+
+            foreach (int broj in UplaceniBrojevi)
+            {
+                if (DobitniBrojevi.Contains(broj) == true)
+                {
+                    pogodak++;
+                }
+            }
+            return pogodak;
+        }  
+
+    }
+}
